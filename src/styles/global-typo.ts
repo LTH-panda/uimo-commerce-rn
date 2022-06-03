c02160c30d149b06f658eb51617d9886c77fd5f0
LTH <johtta888@naver.com>
2022-06-03 23:31:50 +0900

    Design: 추가 global style
    global-color와 global-typo 추가

---
+++ src/styles/global-typo.ts
@@ -0,0 +1,46 @@
+import {StyleSheet} from 'react-native';
+
+const globalType = StyleSheet.create({
+  h0B: {
+    fontSize: 48,
+    fontWeight: '600',
+  },
+  h1B: {
+    fontSize: 32,
+    fontWeight: '600',
+  },
+  h2B: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  body0B: {
+    fontSize: 18,
+    fontWeight: '500',
+  },
+  body1B: {
+    fontSize: 14,
+    fontWeight: '500',
+  },
+  h0R: {
+    fontSize: 48,
+    fontWeight: '400',
+  },
+  h1R: {
+    fontSize: 32,
+    fontWeight: '400',
+  },
+  h2R: {
+    fontSize: 24,
+    fontWeight: '400',
+  },
+  body0R: {
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  body1R: {
+    fontSize: 16,
+    fontWeight: '400',
+  },
+});
+
+export default globalType;
